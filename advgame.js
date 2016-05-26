641ad45fa9b33ad213ea08c2358e8d777bbefdb6
emmakpond <emmapond@students.berkeley.net>
2016-05-26 11:02:04 -0700

    hgt

--- advgame.js
+++ advgame.js
@@ -1,4 +1,21 @@
+//list and function for sandwich scramble//
+var foods=['Penne Pasta', 'Parmesan Cheese', 'Anchovies', 'Kale', 'Cilantro', 'Bread', 'Cod', 'Turkey', 'Sliced Chicken', 'Apple Slices', 'Mustard', 'Mayonnaise', 'Ketchup', 'Chocolate', 'Shrimp', 'Hot Sauce', 'Burger', 'Cheddar Cheese', 'Pine Nuts', 'Sesame Seeds', 'Cashews', 'Tuna']
+Array.prototype.shuffle = function() {
+    var input = this;
+
+    for (var i = input.length-1; i >=0; i--) {
+
+        var randomIndex = Math.floor(Math.random()*(i+1));
+        var itemAtIndex = input[randomIndex];
+
+        input[randomIndex] = input[i];
+        input[i] = itemAtIndex;
+    }
+    return input;
+};
+//start adventure game js//
 $(document).ready(function(){
+    var body=$("body");
     $('#BtnStartPage').click(function(){
         $("#StartPage").addClass('Hidden');
         $("#Home").removeClass('Hidden');
@@ -6,13 +23,14 @@ $(document).ready(function(){
     $('#Btn1a').click(function(){
         $("#Home").addClass('Hidden');
         $("#Lunch").removeClass('Hidden');
-        $("body").css("background-image", "url('bhs.jpg')")
-        $("body").css("background-size", "cover")
+        $(body).css("background-image", "url('bhs.jpg')");
+        $(body).css("background-size", "contain")
     });
     $('#Btn1b').click(function(){
         $("#Home").addClass('Hidden');
         $("#NotBHS").removeClass('Hidden');
-        $("body").css("background-image", "url('shattuck.jpg')")
+        $(body).css("background-image", "url('shattuck2.jpg')");
+        $(body).css("background-size", "cover")
     });
     $('#Btn2a2, #Btn2a3, #Btn2a4, #Btn2a7').click(function(){
         $("#Lunch").addClass('Hidden');
@@ -20,17 +38,17 @@ $(document).ready(function(){
     });
     $('#Btn2a1, #Btn2a6').click(function(){
         $("#Lunch").addClass('Hidden');
-        $("#PartSandwich1").removeClass('Hidden');
-        $("body").css("background-image", "url('sandwich.png')");
+        $("#Sandwich1").removeClass('Hidden');
+        $(body).css("background-image", "url('sandwich.png')");
     });
     $('#Btn2a2').click(function(){
-        $("body").css("background-image", "url('burgerm.jpg')");
+        $(body).css("background-image", "url('burgerm.jpg')");
     });
     $('#Btn2a3').click(function(){
-        $("body").css("background-image", "url('clem.jpg')");
+        $(body).css("background-image", "url('clem.jpg')");
     });
     $('#Btn2a4').click(function(){
-        $("body").css("background-image", "url('arinell1.jpg')");
+        $(body).css("background-image", "url('arinell1.jpg')");
     });
     $('#Btn3a1').click(function(){
         $("#Lunch2").addClass('Hidden');
@@ -38,7 +56,7 @@ $(document).ready(function(){
     });
     $('#Btn3a2').click(function(){
         $("#Lunch2").addClass('Hidden');
-        $("Lunch-Over").removeClass('Hidden');
+        $("#Lunch-Over").removeClass('Hidden');
     });
     $('#Btn4a1').click(function(){
         $("#Part4a2").addClass('Hidden');
@@ -47,12 +65,12 @@ $(document).ready(function(){
     $('#Btn2b1').click(function(){
         $("#NotBHS").addClass('Hidden');
         $("#Dimension").removeClass('Hidden');
-        $("body").css("background-image", "none");
+        $(body).css("background-image", "url('dimension.jpg')");
     });
     $('#Btn2b2').click(function(){
         $("#NotBHS").addClass('Hidden');
         $("#White-House1").removeClass('Hidden');
-        $("body").css("background-image", "none");
+        $(body).css("background-image", "none");
     });
     $('#Btn3b1').click(function(){
         $("#Part3b1").addClass('Hidden');
@@ -67,7 +85,121 @@ $(document).ready(function(){
         $("#Part4b").addClass('Hidden');
         $("#Part4b2").removeClass('Hidden');
     });
+    $("#sandwichscramble").click(function(){
+        $("#Sandwich1").addClass("Hidden");
+        $(".startmessage").removeClass("Hidden");
+        $(body).css("background-image", "url('restaurant-carousel-1.jpg')");
+        $(body).css("background-size", "1500px")
+    });
     $('#playagain').click(function(){
+        location.reload();
+    });
+
+
+    //sandwich scramble js//
+    var indexes= [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
+    var chosen=[];
+    for (var i=0; i<5; i++){
+        chosen[i]=indexes[Math.floor((Math.random()*indexes.length))];
+        indexes.splice(indexes.indexOf(chosen[i]),1);
+    }
+    var firstingredient=foods[chosen[0]];
+    var secondingredient=foods[chosen[1]];
+    var thirdingredient=foods[chosen[2]];
+    var fourthingredient=foods[chosen[3]];
+    var fifthingredient=foods[chosen[4]];
+    $("#firstitem").text(firstingredient);
+    $("#seconditem").text(secondingredient);
+    $("#thirditem").text(thirdingredient);
+    $("#fourthitem").text(fourthingredient);
+    $("#fifthitem").text(fifthingredient);
+
+    for (var e=1;e<6;e++){
+        var indexeswrong=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
+        indexeswrong.splice(chosen[e-1], 1);
+        indexeswrong.shuffle();
+
+        for(var v=1;v<5;v++) {
+
+            $("#wrong" + e.toString() + v.toString()).text(foods[indexeswrong[v]]);
+
+        }
+    }
+    var firstright=$("#firstright");
+    var secondright=$("#secondright");
+    var thirdright=$("#thirdright");
+    var fourthright=$("#fourthright");
+    var win=$("#win");
+    $(firstright).text(firstingredient);
+    $(secondright).text(secondingredient);
+    $(thirdright).text(thirdingredient);
+    $(fourthright).text(fourthingredient);
+    $(win).text(fifthingredient);
+
+    $('#start').click(function(){
+        $(".startmessage").addClass("Hidden");
+        $(".order").removeClass("Hidden");
+        $(body).css("background-image", "url('menu.jpg')");
+        $(body).css("background-size", "contain")
+    });
+    $('#readyss').click(function(){
+        $(".order").addClass("Hidden");
+        $(".firstingredient").removeClass("Hidden");
+        $(body).css("background-image", "url('plate.jpg')");
+        $(body).css("background-size", "cover")
+    });
+    $(firstright).click(function(){
+        $(".firstingredient").addClass("Hidden");
+        $(".secondingredient").removeClass("Hidden");
+    });
+    $('.wrong1').click(function(){
+        $(".firstingredient").addClass("Hidden");
+        $(".wrongmessage").removeClass("Hidden");
+    });
+    $('.wrong2').click(function(){
+        $(".secondingredient").addClass("Hidden");
+        $(".wrongmessage").css("display", "inline");
+    });
+    $('.wrong3').click(function() {
+        $(".thirdingredient").css("display", "none");
+        $(".wrongmessage").css("display", "inline");
+    });
+    $('.wrong4').click(function() {
+        $(".fourthingredient").css("display", "none");
+        $(".wrongmessage").css("display", "inline");
+    });
+    $('.wrong5').click(function() {
+        $(".fifthingredient").css("display", "none");
+        $(".wrongmessage").css("display", "inline");
+    });
+    $('.wrong').click(function(){
+        $(body).css("background-image", "url('face-palm.jpg')");
+        $(body).css("background-size", "1300px")
+    });
+    $(secondright).click(function() {
+        $(".secondingredient").addClass("Hidden");
+        $(".thirdingredient").removeClass("Hidden");
+    });
+    $(thirdright).click(function() {
+        $(".thirdingredient").addClass("Hidden");
+        $(".fourthingredient").removeClass("Hidden");
+    });
+    $(fourthright).click(function() {
+        $(".fourthingredient").addClass("Hidden");
+        $(".fifthingredient").removeClass("Hidden");
+    });
+    $(win).click(function() {
+        $(".fifthingredient").addClass("Hidden");
+        $(".win").removeClass("Hidden");
+        $("body").css("background-image", "url('fw.jpg')")
+    });
+    $('.sameorder').click(function() {
+        $("#wrong").addCLass("Hidden");
+        $(".win").addClass("Hidden");
+        $(".startmessage").removeClass("Hidden");
+        $("body").css("background-image", "url('restaurant-carousel-1.jpg')")
+    });
+    $('.neworder').click(function(){
         location.reload();
     })
 });
--- sandwichscramble.js
+++ sandwichscramble.js
@@ -11,88 +11,55 @@ Array.prototype.shuffle = function() {
         input[i] = itemAtIndex;
     }
     return input;
-}
+};
 $(document).ready(function(){
-    var indexes= [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
-    var firstindex=indexes[Math.floor((Math.random()*indexes.length))];
-    indexes.splice(indexes.indexOf(firstindex),1);
-    var secondindex=indexes[Math.floor((Math.random()*indexes.length))];
-    indexes.splice(indexes.indexOf(secondindex),1);
-    var thirdindex=indexes[Math.floor((Math.random()*indexes.length))];
-    indexes.splice(indexes.indexOf(thirdindex),1);
-    var fourthindex=indexes[Math.floor((Math.random()*indexes.length))];
-    indexes.splice(indexes.indexOf(fourthindex),1);
-    var fifthindex=indexes[Math.floor((Math.random()*indexes.length))];
-    indexes.splice(indexes.indexOf(fifthindex),1);
-    var firstingredient=foods[firstindex];
-    var secondingredient=foods[secondindex];
-    var thirdingredient=foods[thirdindex];
-    var fourthingredient=foods[fourthindex];
-    var fifthingredient=foods[fifthindex];
+    var body=$("body");
+    var indexes= [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
+    var chosen=[];
+    for (var i=0; i<5; i++){
+        chosen[i]=indexes[Math.floor((Math.random()*indexes.length))];
+        indexes.splice(indexes.indexOf(chosen[i]),1);
+    }
+    var firstingredient=foods[chosen[0]];
+    var secondingredient=foods[chosen[1]];
+    var thirdingredient=foods[chosen[2]];
+    var fourthingredient=foods[chosen[3]];
+    var fifthingredient=foods[chosen[4]];
     document.getElementById('firstitem').innerHTML=firstingredient;
     document.getElementById('seconditem').innerHTML=secondingredient;
     document.getElementById('thirditem').innerHTML=thirdingredient;
     document.getElementById('fourthitem').innerHTML=fourthingredient;
     document.getElementById('fifthitem').innerHTML=fifthingredient;
-    document.getElementById('wrong11').innerHTML=foods[Math.floor((Math.random()*foods.length))];
 
-    var indexeswrong1=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
-    indexeswrong1.splice(firstindex, 1);
-    indexeswrong1.shuffle();
-//    $("#wrong11").text(foods[indexeswrong1[1]]);
-    document.getElementById('wrong11').innerHTML=foods[indexeswrong1[1]];
-    document.getElementById('wrong12').innerHTML=foods[indexeswrong1[2]];
-    document.getElementById('wrong13').innerHTML=foods[indexeswrong1[3]];
-    document.getElementById('wrong14').innerHTML=foods[indexeswrong1[4]];
+    for (var e=1;e<6;e++){
+        var indexeswrong=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
+        indexeswrong.splice(chosen[e-1], 1);
+        indexeswrong.shuffle();
 
-    var indexeswrong2=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
-    indexeswrong2.splice(secondindex, 1);
-    indexeswrong2.shuffle();
-    document.getElementById('wrong21').innerHTML=foods[indexeswrong2[1]];
-    document.getElementById('wrong22').innerHTML=foods[indexeswrong2[2]];
-    document.getElementById('wrong23').innerHTML=foods[indexeswrong2[3]];
-    document.getElementById('wrong24').innerHTML=foods[indexeswrong2[4]];
+        for(var v=1;v<5;v++) {
 
-    var indexeswrong3=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
-    indexeswrong3.splice(thirdindex, 1);
-    indexeswrong3.shuffle();
-    document.getElementById('wrong31').innerHTML=foods[indexeswrong3[1]];
-    document.getElementById('wrong32').innerHTML=foods[indexeswrong3[2]];
-    document.getElementById('wrong33').innerHTML=foods[indexeswrong3[3]];
-    document.getElementById('wrong34').innerHTML=foods[indexeswrong3[4]];
+            $("#wrong" + e.toString() + v.toString()).text(foods[indexeswrong[v]]);
 
-    var indexeswrong4=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
-    indexeswrong4.splice(fourthindex, 1);
-    indexeswrong4.shuffle();
-    document.getElementById('wrong41').innerHTML=foods[indexeswrong4[1]];
-    document.getElementById('wrong42').innerHTML=foods[indexeswrong4[2]];
-    document.getElementById('wrong43').innerHTML=foods[indexeswrong4[3]];
-    document.getElementById('wrong44').innerHTML=foods[indexeswrong4[4]];
+        }
+    }
+    document.getElementById('firstright').innerHTML=firstingredient;
+    document.getElementById('secondright').innerHTML=secondingredient;
+    document.getElementById('thirdright').innerHTML=thirdingredient;
+    document.getElementById('fourthright').innerHTML=fourthingredient;
+    document.getElementById('win').innerHTML=fifthingredient;
 
-    var indexeswrong5=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
-    indexeswrong5.splice(fifthindex, 1);
-    indexeswrong5.shuffle();
-    document.getElementById('wrong51').innerHTML=foods[indexeswrong5[1]];
-    document.getElementById('wrong52').innerHTML=foods[indexeswrong5[2]];
-    document.getElementById('wrong53').innerHTML=foods[indexeswrong5[3]];
-    document.getElementById('wrong54').innerHTML=foods[indexeswrong5[4]];
 
-    document.getElementById('firstright').innerHTML=firstingredient
-    document.getElementById('secondright').innerHTML=secondingredient
-    document.getElementById('thirdright').innerHTML=thirdingredient
-    document.getElementById('fourthright').innerHTML=fourthingredient
-    document.getElementById('win').innerHTML=fifthingredient
     $('#start').click(function(){
         $(".startmessage").css("display", "none");
         $(".order").css("display", "inline");
-        $("body").css("background-image", "url('menu.jpg')")
-        $("body").css("background-size", "contain")
+        $(body).css("background-image", "url('menu.jpg')");
+        $(body).css("background-size", "contain")
     });
     $('#readyss').click(function(){
         $(".order").css("display", "none");
         $(".firstingredient").css("display", "inline");
-        $("body").css("background-image", "url('plate.jpg')")
-        $("body").css("background-size", "cover")
+        $(body).css("background-image", "url('plate.jpg')");
+        $(body).css("background-size", "cover")
     });
     $('#firstright').click(function(){
         $(".firstingredient").css("display", "none");
@@ -119,9 +86,9 @@ $(document).ready(function(){
         $("#wrong").css("display", "inline");
     });
     $('.wrong').click(function(){
-        $("body").css("background-image", "url('face-palm.jpg')")
-        $("body").css("background-size", "1300px")
-    })
+        $(body).css("background-image", "url('face-palm.jpg')");
+        $(body).css("background-size", "1300px")
+    });
     $('#secondright').click(function() {
         $(".secondingredient").css("display", "none");
         $(".thirdingredient").css("display", "inline");
@@ -142,7 +109,7 @@ $(document).ready(function(){
     $('.sameorder').click(function() {
         $("#wrong").css("display", "none");
         $(".win").css("display", "none");
-        $(".startmessage").css("display", "inline")
+        $(".startmessage").css("display", "inline");
         $("body").css("background-image", "url('restaurant-carousel-1.jpg')")
     });
     $('.neworder').click(function(){
