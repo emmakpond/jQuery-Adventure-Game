6a4995b8477d89747881eab12bee6085aafef882
emmakpond <emmapond@students.berkeley.net>
2016-06-01 11:38:03 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Snake.css
    #	advgame.html
    #	css/Snake.css
    #	image/white-house.jpg
    #	minigames.css

---
+++ javascript/advgame.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+    var body=$("body");
+    $('#Btn1a').click(function(){
+        $("#Home").fadeOut();
+        $("#Lunch").delay(500).fadeIn();
+        $(body).css("background-image", "url('../image/bhs.jpg')");
+        $(body).css("background-size", "cover")
+    });
+    $('#Btn1b').click(function(){
+        $("#Home").fadeOut();
+        $("#NotBHS").delay(500).fadeIn();
+        $(body).animate({backgroundColor: black});
+    });
+    $('#Btn2a2, #Btn2a3, #Btn2a4, #Btn2a7').click(function(){
+        $("#Lunch").fadeOut();
+        $("#Lunch2").delay(500).fadeIn();
+    });
+    $('#Btn2a1, #Btn2a6').click(function(){
+        $("#Lunch").fadeOut();
+        $("#Sandwich1").delay(500).fadeIn();
+        $(body).css("background-image", "url('../image/sandwich.png')");
+    });
+    $('#Btn2a2').click(function(){
+        $(body).css("background-image", "url('../image/burgerm.jpg')");
+    });
+    $('#Btn2a3').click(function(){
+        $(body).css("background-image", "url('../image/clem.jpg')");
+    });
+    $('#Btn2a4').click(function(){
+        $(body).css("background-image", "url('../image/arinell1.jpg')");
+    });
+    $('#Btn3a1').click(function(){
+        $("#Lunch2").fadeOut();
+        $("#Line-Long").delay(500).fadeIn();
+    });
+    $('#Btn3a2').click(function(){
+        $("#Lunch2").fadeOut();
+        $("#Lunch-Over").delay(500).fadeIn();
+    });
+    $('#Btn4a1').click(function(){
+        $("#Part4a2").fadeOut();
+        $("#Part5a1").delay(500).fadeIn();
+    });
+    $('#Btn2b1').click(function(){
+        $("#NotBHS").fadeOut();
+        $("#Dimension").delay(500).fadeIn();
+        $(body).css("background-image", "url('../image/dimension.jpg')");
+    });
+    $('#Btn2b2').click(function(){
+        $("#NotBHS").fadeOut();
+        $("#White-House1").delay(500).fadeIn();
+        $(body).css("background-image", "url('../image/whitehouse.jpg')");
+    });
+    $('#Btn3b1').click(function(){
+        $("#Part3b1").fadeOut();
+        $("#Part4b1").delay(500).fadeIn();
+    });
+    $('#Btn4b1').click(function(){
+        $("#Dimension").fadeOut();
+        $("#Game-Snake").delay(500).fadeIn();
+        $("#Dimension").addClass('Hidden');
+        $("#Game-Snake").removeClass('Hidden');
+        $(body).css("background-image", "none");
+        $(body).css("background-color", "white");
+        $("*").css("padding", "0 0 0 0");
+    });
+    $('#Btn4b2').click(function(){
+        $("#Part4b").fadeOut();
+        $("#Part4b2").delay(500).fadeIn();
+    });
+    $("#sandwichscramble").click(function(){
+        $("#Sandwich1").fadeOut();
+        $(".startmessage").delay(500).fadeIn();
+        $(body).css("background-image", "url('../image/restaurant-carousel-1.jpg')");
+        $(body).css("background-size", "1500px")
+    });
+    $('#playagain').click(function(){
+        location.reload();
+    });
+});
